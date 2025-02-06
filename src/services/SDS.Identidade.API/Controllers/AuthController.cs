a0a4b44ace143e1d2c54775dc40eba9ba02d33d1
David Santos <david.santos@finanblue.com.vc>
2025-02-06 13:35:11 -0300

    Adicionado um Custom Response  para respostas padronizadas

--- src/services/SDS.Identidade.API/Controllers/AuthController.cs
+++ src/services/SDS.Identidade.API/Controllers/AuthController.cs
@@ -11,7 +11,7 @@ using System.Text;
 namespace SDS.Identidade.API.Controllers;
 
 [ApiController]
-public class AuthController : Controller
+public class AuthController : MainController
 {
     private readonly SignInManager<IdentityUser> _signInManager;
     private readonly UserManager<IdentityUser> _userManager;
@@ -29,7 +29,7 @@ public class AuthController : Controller
     [HttpPost("nova-conta")]
     public async Task<ActionResult> Registrar(UsuarioRegistro usuarioRegistro)
     {
-        if (!ModelState.IsValid) return BadRequest();
+        if (!ModelState.IsValid) return CustomResponse(ModelState);
 
         var user = new IdentityUser
         {
@@ -42,26 +42,37 @@ public class AuthController : Controller
 
         if (result.Succeeded)
         {
-            await _signInManager.SignInAsync(user, false);
-            return Ok(await GerarJwt(usuarioRegistro.Email));
+            return CustomResponse(await GerarJwt(usuarioRegistro.Email));
         }
 
-        return BadRequest();
+        foreach (var erro in result.Errors)
+        {
+            AdicionarErroProcessamento(erro.Description);
+        }
+
+        return CustomResponse();
     }
 
     [HttpPost("autenticar")]
     public async Task<ActionResult> Login(UsuarioLogin usuarioLogin)
     {
-        if (!ModelState.IsValid) return BadRequest();
+        if (!ModelState.IsValid) return CustomResponse(ModelState);
 
         var result = await _signInManager.PasswordSignInAsync(usuarioLogin.Email, usuarioLogin.Senha, false, true);
 
         if (result.Succeeded)
         {
-            return Ok(await GerarJwt(usuarioLogin.Email));
+            return CustomResponse(await GerarJwt(usuarioLogin.Email));
         }
 
-        return BadRequest();
+        if(result.IsLockedOut)
+        {
+            AdicionarErroProcessamento("Usuário temporariamente bloqueado por tentativas inválidas");
+            return CustomResponse();
+        }
+
+        AdicionarErroProcessamento("Usuário ou senha incorretos");
+        return CustomResponse();
     }
 
     private async Task<UsuarioRespostaLogin> GerarJwt(string email)
